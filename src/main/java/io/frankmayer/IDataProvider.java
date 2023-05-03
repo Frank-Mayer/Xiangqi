9a953bb8bad8aba24c2dc81076f09b42ee22dfae
Frank-Mayer <git@frank-mayer.io>
2023-05-03 13:05:36 +0200

    added moves to data interface

--- src/main/java/io/frankmayer/IDataProvider.java
+++ src/main/java/io/frankmayer/IDataProvider.java
@@ -1,9 +1,12 @@
 package io.frankmayer;
 
 import java.util.Map;
+import java.util.Set;
 
 public interface IDataProvider {
-    Map<Integer, Character> getCharacters();
+  Map<Integer, Character> getCharacters();
 
-    void move(int source, int target);
+  void move(int source, int target);
+
+  Set<Integer> getMoves(int source);
 }
